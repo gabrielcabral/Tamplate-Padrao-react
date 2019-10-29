1a2f8248e58ed9fcda79b82eff79b4ef36960bd1
gabriel.cabral <cabraldealmeida@gmail.com>
2019-10-29 10:29:28 -0300

    finalizando tamplate

---
+++ src/pages/Main/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+ import { Title } from './styles';
+
+export default function Main() {
+  return (
+    <div className="App">
+    <Title>Main</Title>
+      <Title>Tamplate Padrão React:</Title>
+      <ul>
+        <li>Rotas</li>
+        <li>React-dom</li>
+        <li>editorconfig</li>
+        <li>Eslint</li>
+        <li>Prettier</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,9 @@
+import  styled  from 'styled-components';
+
+export const Title = styled.h1`
+  font-family:Arial, Helvetica, sans-serif;
+  color: #7159c1;
+  font-size:24px;
+`;
+
+
