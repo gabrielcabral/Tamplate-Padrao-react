aeafe037635ff94f38059ecfa0de5c7c9bec4228
gabriel.cabral <cabraldealmeida@gmail.com>
2019-10-29 15:45:15 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-import React , { useState , useEffect, useMemo , useCallback} from 'react';
+import React from 'react';
 
-
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
-  const [tech , setTech] = useState([
-  ]);
-  const [newTech , setNewTech] = useState('');
-
-  const addTech = useCallback(() =>{
-    setTech([...tech, newTech ]);
-    setNewTech('');
-    setNewTech('');
-    setNewTech('');
-  },[ newTech , tech ]);
-
- useEffect(() => {
-   const storageTech = localStorage.getItem('tech');
-   if(storageTech){
-     setTech(JSON.parse(storageTech));
-   }
- }, []);
-
-  useEffect(() => {
-    localStorage.setItem('tech', JSON.stringify(tech));
-  }, [tech]);
-
-
- const teckSize =  useMemo(() => tech.length, [tech]);
-
   return (
     <>
-    <ul>
-      { tech.map( t=>(
-        <li key={t}>{t}</li>
-      ))}
-
-    </ul>
-    <strong> você tem {teckSize} Tecnologias</strong><br></br>
-
-    <input type="text" onChange={e => setNewTech(e.target.value)}/>
-    <button type="button" onClick={addTech}>Adiccionar</button>
+    <Routes/>
+    <GlobalStyle/>
     </>
   );
 }
