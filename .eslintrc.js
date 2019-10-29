d568b16aad7939eefcd74172d6e8fd443d8850f8
gabriel.cabral <cabraldealmeida@gmail.com>
2019-10-29 15:28:19 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     'react',
     'prettier',
+    'react-hooks',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -30,7 +31,10 @@ module.exports = {
       'warn',
       {extension: ['.jsx', '.js']}
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', {allow:['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
 
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React , { useState , useEffect, useMemo , useCallback} from 'react';
 
-import Routes from './routes';
-import GlobalStyle from './styles/global';
+
 
 function App() {
+  const [tech , setTech] = useState([
+  ]);
+  const [newTech , setNewTech] = useState('');
+
+  const addTech = useCallback(() =>{
+    setTech([...tech, newTech ]);
+    setNewTech('');
+    setNewTech('');
+    setNewTech('');
+  },[ newTech , tech ]);
+
+ useEffect(() => {
+   const storageTech = localStorage.getItem('tech');
+   if(storageTech){
+     setTech(JSON.parse(storageTech));
+   }
+ }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+
+ const teckSize =  useMemo(() => tech.length, [tech]);
+
   return (
     <>
-    <Routes/>
-    <GlobalStyle/>
+    <ul>
+      { tech.map( t=>(
+        <li key={t}>{t}</li>
+      ))}
+
+    </ul>
+    <strong> você tem {teckSize} Tecnologias</strong><br></br>
+
+    <input type="text" onChange={e => setNewTech(e.target.value)}/>
+    <button type="button" onClick={addTech}>Adiccionar</button>
     </>
   );
 }
